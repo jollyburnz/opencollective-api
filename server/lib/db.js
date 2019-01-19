acf3dd56a3bc3bd865efecf9980e5323fc7c2742
Jackson Lin <think.jackson@gmail.com>
2019-01-18 20:38:17 -0500

    updated database url

--- server/lib/db.js
+++ server/lib/db.js
@@ -56,6 +56,7 @@ export function getDBConf(name) {
     const dbConfigOverride = get(config, [name, 'override'], {});
     dbConfig = { ...dbConfig, ...dbConfigOverride };
   }
+  console.log(dbConfig);
   return dbConfig;
 }
 
